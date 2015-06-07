63f56103439b8816a034952ad9d209ef0776aa70
sstrick22 <sstrick22@gmail.com>
2015-06-07 01:09:25 -0500

    Update from master

--- index.html_bootstrap.dart.js
+++ index.html_bootstrap.dart.js
@@ -8576,7 +8576,7 @@ z=J.Vg((a.shadowRoot||a.webkitShadowRoot).querySelector("#click_header"))
 H.J(new W.Ov(0,z.Q,z.a,W.VF(new A.wQ(a)),z.b),[H.Kp(z,0)]).YI()
 z=J.Vg((a.shadowRoot||a.webkitShadowRoot).querySelector("#import_header"))
 H.J(new W.Ov(0,z.Q,z.a,W.VF(new A.zP(a)),z.b),[H.Kp(z,0)]).YI()
-W.Kn("configurations/original.json",null,null).Rx(this.gEC(a),new A.Wf(a))},
+W.Kn("configurations/conway.json",null,null).Rx(this.gEC(a),new A.Wf(a))},
 Mk:[function(a,b){var z,y,x,w
 if(a.eD===!0)this.Db(a)
 try{y=Z.Ep(b)
